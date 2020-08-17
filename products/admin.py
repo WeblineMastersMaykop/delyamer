1314fdc8da882b73a5a1043d86ed23b754fa60f7
Gurgen <goga23d@gmail.com>
2020-08-17 23:31:23 +0300

    Начал делать товары

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_mptt_admin.admin import DjangoMpttAdmin
-from products.models import Color, Size, Category, Product
+from products.models import Color, Size, Category, Product, Offer, OfferImage
 
 
 admin.site.register(Size)
@@ -28,12 +28,23 @@ class ColorAdmin(admin.ModelAdmin):
     list_display = ('name', 'color')
 
 
+class OfferImageInline(admin.TabularInline):
+    model = OfferImage
+    extra = 0
+    classes = ('grp-collapse grp-closed',)
+
+
+class OfferInline(admin.TabularInline):
+    model = Offer
+    extra = 0
+    classes = ('grp-collapse grp-closed',)
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {
-            'fields': ('category', 'name', 'price', 'sale', 'is_new', 'is_bs', 'is_active'),
+            'fields': ('category', 'name', 'vendor_code', 'price', 'desc', 'is_new', 'is_bs', 'is_active'),
         }),
         ('SEO', {
             'classes': ('grp-collapse grp-closed',),
@@ -41,6 +52,20 @@ class ProductAdmin(admin.ModelAdmin):
         }),
     )
 
-    list_display = ('name', 'category', 'price', 'sale', 'is_new', 'is_bs', 'is_active', 'created', 'updated')
+    list_display = ('name', 'category', 'price', 'vendor_code', 'is_new', 'is_bs', 'is_active', 'created', 'updated')
     list_editable = ('is_active', 'is_new', 'is_bs')
+    search_fields = ('name', 'vendor_code', 'category__name', 'desc')
     prepopulated_fields = {'slug': ('name',)}
+    inlines = (OfferInline, OfferImageInline)
+
+
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('product', 'color', 'size', 'stock', 'sale', 'is_active'),
+        }),
+    )
+
+    list_display = ('product', 'color', 'size', 'stock', 'sale', 'is_active', 'created', 'updated')
+    list_editable = ('is_active',)
--- products/models.py
+++ products/models.py
@@ -3,6 +3,9 @@ import mptt
 from mptt.models import MPTTModel, TreeForeignKey
 from colorfield.fields import ColorField
 from core.models import SEO, Position
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+from uuid import uuid1
 
 
 class Category(MPTTModel, SEO, Position):
@@ -44,11 +47,22 @@ class Size(models.Model):
         return self.name
 
 
+class Property(models.Model):
+    # TODO Остановился тут
+    class Meta:
+        verbose_name = 'Характеристика'
+        verbose_name_plural = 'Характеристики'
+
+    def __str__(self):
+        return self.name
+
+
 class Product(SEO):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='Категория')
     name = models.CharField('Название', max_length=250)
+    vendor_code = models.CharField('Артикул', max_length=20)
     price = models.PositiveIntegerField('Цена')
-    sale = models.PositiveIntegerField('Скидка', default=0)
+    desc = models.TextField('Описание', null=True, blank=True)
     is_active = models.BooleanField('Показывать на сайте', default=True)
     is_new = models.BooleanField('NEW', default=True)
     is_bs = models.BooleanField('ХИТ', default=True)
@@ -61,3 +75,55 @@ class Product(SEO):
 
     def __str__(self):
         return self.name
+
+
+class Offer(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='offers', verbose_name='Товар')
+    color = models.ForeignKey(Color, on_delete=models.SET_NULL, related_name='offers', verbose_name='Цвет', null=True, blank=True)
+    size = models.ForeignKey(Size, on_delete=models.SET_NULL, related_name='offers', verbose_name='Размер', null=True, blank=True)
+    stock = models.PositiveIntegerField('На складе', default=0)
+    sale = models.PositiveIntegerField('Скидка', default=0)
+    is_active = models.BooleanField('Показывать на сайте', default=True)
+    created = models.DateField('Дата создания', auto_now_add=True)
+    updated = models.DateField('Дата изменения', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Вариант товара'
+        verbose_name_plural = 'Варианты товара'
+
+    def __str__(self):
+        return '{0} -- {1} -- {2}'.format(self.product.name, self.color.name, self.size.name)
+
+
+class OfferImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images', verbose_name='Товар')
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE, related_name='images', verbose_name='Вариант товара')
+
+    def get_picture_url(self, filename):
+        ext = filename.split('.')[-1]
+        filename = '{0}.{1}'.format(uuid1(), ext)
+        return 'images/products/{0}/{1}'.format(self.offer.product.slug, filename)
+
+    image = models.ImageField(upload_to=get_picture_url, verbose_name='Изображение')
+
+    image_big = ImageSpecField(source='image',
+                                 processors=[ResizeToFill(696, 870)],
+                                 format='JPEG',
+                                 options={'quality': 90})
+
+    image_medium = ImageSpecField(source='image',
+                                 processors=[ResizeToFill(260, 391)],
+                                 format='JPEG',
+                                 options={'quality': 90})
+
+    image_small = ImageSpecField(source='image',
+                                 processors=[ResizeToFill(648, 110)],
+                                 format='JPEG',
+                                 options={'quality': 90})
+
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения'
+
+    def __str__(self):
+        return '{0} -- {1}'.format(self.offer.product.name, self.id)
